aa0a10f82954384d85becc80ddbe487e74773c5f
kaerukuujo <mrjolyne@gmail.com>
2022-11-17 12:11:11 +0000

    added logic to return rock paper scissors randomly

--- script.js
+++ script.js
@@ -4,16 +4,55 @@ function getRandomNum(min, max) {
 
 function getComputerChoice() {
     let roll = Math.floor(getRandomNum(1, 4));
-    let choice = 'Paper';
-    console.log(roll);
+    let choice = `none`;
 
-    if (roll === 1) {
-        console.log("Paper");
-    } else if (roll === 2) {
-        console.log("Scissors");
+    if (roll == 1) {
+        choice = `rock`;
+    } else if (roll == 2) {
+        choice = `paper`;
     } else {
-        console.log("Rock");
+        choice = `scissors`;
     }
+    
+    return choice;
 }
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+    let winner = " ";
+    let win = " ";
+    const playerChoice = playerSelection.toLowerCase();
+
+    if (playerChoice === computerSelection) {
+        let winner = "noone"
+        
+    } else if (playerChoice == "rock" && computerSelection == "paper") {
+        let winner = "computer";
+    } else if (playerChoice == "rock" && computerSelection == "scissors") {
+        let winner = "player";
+    } else if (playerChoice == "scissors" && computerSelection == "rock") {
+        let winner = "computer";
+    } else if (playerChoice == "scissors" && computerSelection == "paper") {
+        let winner = "player";
+    } else if (playerChoice == "paper" && computerSelection == "scissors") {
+        let winner = "computer";
+    } else if (playerChoice == "paper" && computerSelection == "rock") {
+        let winner = "player";
+    } else { 
+        let winner = "noone";
+    }
+    console.log(playerChoice);
+    console.log(computerSelection);
+
+    if (winner === "noone") {
+        let win = `you both picked ${playerChoice}!, noone wins.`;
+    } else if(winner === "computer") {
+         let win = `You Lose! ${computerSelection} beats ${playerChoice}`;
+    } else {
+        let win = `You win! ${playerChoice} beats ${computerSelection}`
+    }
+
+    return win;
+}
+let computerSelection = getComputerChoice();
+let playerSelection = "rock"; 
+playRound(playerSelection, computerSelection);
